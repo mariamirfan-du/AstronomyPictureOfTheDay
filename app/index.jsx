12af29814c2de8093c3227ef2e756ae29c496708
Mariam Irfan <mariamirfan.du@gmail.com>
2024-07-17 16:01:44 +0500

    4 files UPDATED

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router'
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Link , useRouter } from 'expo-router'
 
 export default function App() {
+  const router = useRouter();
+
+  const handletap = () =>{
+    router.push('/profile')
+  };
+
   return (
-    <View style={styles.container} className='flex-1 items-center justify-center bg-purple'>
-      <Text className="text-3x1">Stargazer's Daily</Text>
-      <StatusBar style="auto" />
-        <Link href="/APOD" style={{ color: 'blue'}}>View Today's</Link>
+    <TouchableWithoutFeedback onPress={handletap}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Stargazer's</Text>
+        <Text style={styles.title}>Daily</Text>
+        <StatusBar style="auto" />
+        <Text style= {styles.tapText}>Tap anywhere to continue</Text>
     </View>
+      
+    </TouchableWithoutFeedback>
+
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2B2436',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
+  title:{
+    fontSize: 40,
+    color: '#99EBDC',
+    fontWeight: 'bold',
+    mariginBottom:10,
+  },
+  tapText: {
+    marginTop:20,
+    fontSize: 14,
+    color: '#99EBDC',
+    textAlign:'center',
+    bottom: 100,
+    position: 'absolute'
+
+
+  },
+
 });
--- app/profile.jsx
+++ app/profile.jsx
@@ -1,14 +1,137 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React , {useState} from 'react'
+import { Link } from 'expo-router'
+import { getFormatedDate } from 'react-native-modern-datepicker';
+import { Calendar } from 'react-native-calendars';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 const profile = () => {
+
+  const today = new Date();
+
+  const startDate = getFormatedDate(today.setDate(today.getDate() + 1) , 'YYYY/MM/DD')
+
+  const [open , setOpen] = useState(false) // open and closes the modal
+  const [date , setDate] = useState() // date variable
+
+  function handleOnPress(){
+    setOpen(!open);
+  }
+
+  function handleChange(propDate){
+    setDate(propDate)
+  }
+
+
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Calendar</Text>
+          <SafeAreaView>
+
+          <Calendar
+          //minimumDate={startDate}
+          style={styles.calendar}
+          theme={{
+             monthTextColor: '#2B2436',
+             textMonthFontSize: 30,
+             textMonthFontWeight: 'bold',
+             arrowColor: '#2B2436',
+             calendarBackground:'white',
+             todayTextColor: '#00adf5',
+             textSectionTitleColor:'purple',
+             textInactiveColor:'purple',
+             textDayFontWeight:'bold',
+             textDisabledColor:'grey',
+             selectedDayBackgroundColor:'#65558F',
+             selectedDayTextColor:'white',
+
+
+
+
+
+
+          }}
+          onMonthChange={() => {}}
+          selected={startDate}
+          onDayPress={day => {
+            setDate(day.dateString);
+          }}
+          markedDates={{
+            [date]: {selected: true, disableTouchEvent: true, selectedDotColor: 'purple'}
+          }}
+          />
+          </SafeAreaView>
+
+          
     </View>
+
   )
 }
 
 export default profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#2B2436',
+    },
+    title:{
+      fontSize: 40,
+      color: '#99EBDC',
+      fontWeight: 'bold',
+      alignSelf:'center',
+      marginBottom:10,
+      marginTop:10,
+      padding:15,
+    },
+    text: {
+      marginTop:30,
+      fontSize: 14,
+      color: '#99EBDC',
+      textAlign:'center',
+      position: 'absolute',
+      marginBottom:5
+    },
+
+    link:{
+      color:'#99EBDC',
+      marginTop:40 
+    },
+
+    modalView:{
+      margin:20,
+      backgroundColor:'white',
+      borderRadius:20,
+      width:'98%',
+      padding:35,
+      alignItems:'center',
+      shadowColor:'#000',
+      shadowOffset:{
+        width:0,
+        height:2,
+      },
+      shadowOpacity:0.25,
+      shadowRadius:4,
+      elevation: 5,
+    },
+
+    centeredView:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      marginTop:22,
+    },
+    calendar:{
+      backgroundColor:'white',
+      borderWidth:1,
+      borderColor:'white',
+      borderRadius:15,
+      padding:35,
+      margin:25,
+
+
+
+    }
+
+});
